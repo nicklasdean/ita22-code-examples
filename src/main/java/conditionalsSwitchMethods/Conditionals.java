b52b131a9b216becb70563c7f2c5e2bf914b65ed
dean <nicklasfrederiksen@gmail.com>
2023-01-30 12:49:18 +0100

    first

--- src/main/java/conditionalsSwitchMethods/Conditionals.java
+++ src/main/java/conditionalsSwitchMethods/Conditionals.java
@@ -2,6 +2,9 @@ package conditionalsSwitchMethods;
 
 public class Conditionals {
     public static void main(String[] args) {
+        //Detail - equality in Strings
+
         //Checks is username is longer than 10 characters
+        //And that it is full lowercase
     }
 }
---
+++ src/main/java/inheritance/User.java
@@ -0,0 +1,30 @@
+package inheritance;
+
+import java.util.Random;
+
+public class User {
+    String username;
+    String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String generateNewPassword(){
+        this.password = "";
+        String newPassword = "";
+        for (int i = 0; i < 10; i++) {
+            newPassword = newPassword.concat(generateRandomLetter());
+        }
+        System.out.println(newPassword);
+        return newPassword;
+    }
+
+    private String generateRandomLetter(){
+        String letters = "abcdefghijklmnopqrstuvxyz";
+        Random randomNumberGenerator = new Random();
+        int randomNumber = randomNumberGenerator.nextInt(letters.length());
+        return String.valueOf(letters.charAt(randomNumber));
+    }
+}
