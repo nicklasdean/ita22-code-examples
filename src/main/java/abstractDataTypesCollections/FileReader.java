ac5c4113be5aecad6f27b92f7980f6154d42dc0e
dean <nicklasfrederiksen@gmail.com>
2023-02-13 10:14:58 +0100

    first

--- src/main/java/abstractDataTypesCollections/FileReader.java
+++ src/main/java/abstractDataTypesCollections/FileReader.java
@@ -1,7 +1,20 @@
 package abstractDataTypesCollections;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileReader {
-    public static void main(String[] args) {
-        //
+    public static void main(String[] args) throws FileNotFoundException {
+        //Refer to the file I want to read
+        File alice = new File("src/main/java/abstractDataTypesCollections/alice-in-wonderland.txt");
+
+        System.out.println(alice.canRead());
+        //A scanner with the file
+        Scanner bookScanner = new Scanner(alice);
+
+        while(bookScanner.hasNext()){
+            System.out.println(bookScanner.next());
+        }
     }
 }
