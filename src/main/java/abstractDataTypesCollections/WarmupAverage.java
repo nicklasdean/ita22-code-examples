165ce4a734e2b87f3dac072e95c8305cb28cd150
dean <nicklasfrederiksen@gmail.com>
2023-02-21 09:10:50 +0100

    first

--- src/main/java/abstractDataTypesCollections/WarmupAverage.java
+++ src/main/java/abstractDataTypesCollections/WarmupAverage.java
@@ -19,4 +19,3 @@ public class WarmupAverage {
         return average;
     }
 }
-
---
+++ src/main/java/sortingSearching/Example.java
@@ -0,0 +1,15 @@
+package sortingSearching;
+
+public class Example {
+    public static void main(String[] args) {
+        //[3,2,1]
+        int[] arrayToSort = {5,2,3,1,3};
+        //Save a value in a temporary variable
+        //Copy one value to slot
+        //[1,2,1]
+        //Insert temporary value in last slot
+        //[1,2,3]
+
+        //Hvordan finder jeg mindste element i et array?
+    }
+}
